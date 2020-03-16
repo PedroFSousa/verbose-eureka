148ceeac29aa8234cd874ff5420512b0478a838b
pedro.f.sousa <pedro.f.sousa@inesctec.pt>
2020-03-16 02:35:04 +0000

    ##dist## try4

--- scripts/ci/ci-tester.sh
+++ scripts/ci/ci-tester.sh
@@ -45,6 +45,9 @@ apt-get install python python3.6 python3.6-dev python3-pip curl libffi-dev
 apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt-get install -y nodejs
+sudo npm install
+sudo npm start
+
 
 # NOTE: Xenial is supposed to already have python3.6.7 installed, but logs say 3.5.3
 
@@ -99,8 +102,8 @@ cd $TESTER_DIR
 
 # still not recognizing npm...
 # will it work this ?
-npm install
-npm start
+sudo npm install
+sudo npm start
 
 if [ $? == 0 ]; then
   echo -e "All tests were successfully completed!\n"
